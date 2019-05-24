cf10e97ce3494a180a385c3282c88202726c8c19
thompsonjonm <thompson.jonm@gmail.com>
2019-05-24 09:50:37 -0400

    Further edits to hooks and constants

---
+++ features/support/constants.rb
@@ -0,0 +1,35 @@
+class Const
+
+    # 
+    # Primary Test URLs
+    # 
+    GOOGLE_HOMEPAGE_URL = 'https://www.google.com'
+
+    # 
+    # Secondary Test URLs
+    # 
+    GMAIL_URL = GOOGLE_HOMEPAGE_URL + '/gmail'
+    LOGIN_URL = 'https://accounts.google.com/signin'
+    SEARCH_RESULTS_URL = GOOGLE_HOMEPAGE_URL + '/search?source='
+
+    # 
+    # Messages
+    # 
+    # Space used for site messages (negative and positive)
+    # eg. INVALID_PASSWORD = 'Please enter a valid password.'
+
+    # 
+    # Users
+    # 
+    GOOGLE_USER = {
+        USERNAME => 'tests@qatesting.com',
+        PASSWORD => 'testing123'
+    }
+
+    # 
+    # Data
+    # 
+    # Space used for testing data
+    # eg. VALID_FILENAME = 'Ruby Test File'
+
+end
