1c3911d85c594c95f425ebbfbf2730cf7c9a06e3
thompsonjonm <thompson.jonm@gmail.com>
2019-06-09 10:15:53 -0400

    Demo QA intro test refactor

--- features/demo_qa/demo_qa_steps.rb
+++ features/demo_qa/demo_qa_steps.rb
@@ -2,14 +2,14 @@ Before do
     @demoqa = DemoQA.new(@browser)
 end
 
-# 
+#
 # Test:
 # Visit the Demo QA Website
-# 
+#
 Given(/^we visit Demo QA$/) do
     @demoqa.visit
 end
 
 Then(/^the Demo QA website should load properly$/) do
-    expect(@browser.title).to eq('ToolsQA – Demo Website to Practice Automation – Demo Website to Practice Automation')
+    expect(@browser.title).to eq(Const::DEMO_QA_PAGE_TITLE)
 end
--- features/support/constants.rb
+++ features/support/constants.rb
@@ -1,33 +1,34 @@
 class Const
 
-    # 
+    #
     # Primary Test URLs
-    # 
+    #
     DEMO_QA_URL = 'https://www.demoqa.com'
 
-    # 
+    #
     # Secondary Test URLs
-    # 
+    #
     DEMO_QA_LINKS = {
         :tooltip => DEMO_QA_URL + '/tooltip'
     }
 
-    # 
+    #
     # Messages
-    # 
+    #
     # Space used for site messages (negative and positive)
     # eg. INVALID_PASSWORD = 'Please enter a valid password.'
 
-    # 
+    #
     # Users
-    # 
+    #
     # Space used for user data
     # eg. USERNAME = 'super@user.com'
 
-    # 
+    #
     # Data
-    # 
+    #
     # Space used for testing data
     # eg. VALID_FILENAME = 'Ruby Test File'
+    DEMO_QA_PAGE_TITLE = 'ToolsQA – Demo Website to Practice Automation – Demo Website to Practice Automation'
 
 end
